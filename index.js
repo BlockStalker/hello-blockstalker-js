ce11cad3e2711fb32d2105ec99f1d3f822578ba2
Eric Kirse <kirse1@gmail.com>
2024-06-13 20:17:30 -0700

    feat: :rocket: sync with client-js 0.2.0; provide updated API examples

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { blockStalkerClient, restClient, FilterFormBuilder, NumericCondition, KeyEvent, KeyIdentity } from '@blockstalker/client-js';
+import { blockStalkerClient, restClient, FilterFormBuilder, NumericCondition, KeyGroup, KeyType, KeyEvent } from '@blockstalker/client-js';
 
 const local_apiKey = 'YOUR_API_KEY_HERE'; 
 // Note: If NodeJS > 20, we recommend using a .env file for your API key
@@ -17,7 +17,7 @@ const mainClient = blockStalkerClient(apiKey, apiBaseURL);
 */
 
 // Customize restClient to set global options (i.e. request/response tracing)
-const rc = restClient(apiKey, apiBaseURL, { trace: false });
+const rc = restClient(apiKey, apiBaseURL, { trace: true });
 
 // Otherwise you can simply use blockStalkerClient:
 const myStreams = await mainClient.rest.streams.owned();
@@ -28,28 +28,35 @@ const streams = await rc.streams.owned();
 const personalStream = streams.personal;
 console.log("Personal stream:", personalStream);
 
+//
 // Create a Key-based filter for USDC (US Dollar Coin) transfers >= 5 USDC
+//
 // USDC = Asset Id: 31566704
 // Note: Key-based filters are processed first, and are a more efficient use of your Match Credits
 const usdc = await rc.registry.lookup('31566704');
 console.log("USDC Asset:", usdc);
 const usdcFilter = new FilterFormBuilder()
-    .asKeyFilter(usdc.blockItem.key, KeyEvent.AssetXfer)
+    .keyFilter('31566704')
+    .event(KeyEvent.AssetXfer)
     .stream(personalStream)
-    .amount(5, NumericCondition.GreaterOrEqual)
+    //.amount(5, NumericCondition.GreaterOrEqual)  // Optionally set a minimum Xfer Amount
     .build();
 
-// Create an Event-based filter for all RandGallery Marketplace Listings
-const randGallery = await rc.registry.lookupKeyId(KeyIdentity.RandGallery);
-console.log("RandGallery:", randGallery);
-const randFilter = new FilterFormBuilder()
-    .asEventFilter(randGallery.whatIsIt.keyIdentity, KeyEvent.MarketplaceListing)
+//
+// Create an Id-based filter for all Central Exchange Withdrawals
+//
+// A KeyIdentity is a pairing of (KeyGroup, KeyType)
+// Note: Id Filters can be busier since they generally represent multiple Keys
+//       Keep an eye on Match Usage: https://blockstalker.io/account
+const cexFilter = new FilterFormBuilder()
+    .idFilter(KeyGroup.CentralExchange, KeyType.Account)
+    .event(KeyEvent.ExchangeWithdrawal)
     .stream(personalStream)
     .build();
 
 // Create the filters
 await rc.filters.create(usdcFilter);
-await rc.filters.create(randFilter);
+await rc.filters.create(cexFilter);
 
 // List all filters for stream
 const filters = await rc.filters.getByStream(personalStream);
